5fc2258fa8d94d05e0e66c62fba5b4d6bc4d7bf2
Lekvf <d.namsaraeva04@gmail.com>
2022-02-14 13:40:51 +0700

    5

--- task2.c
+++ task2.c
@@ -1,6 +1,7 @@
 #include <sys/types.h>
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 
 extern char *tzname[];
 
@@ -10,15 +11,24 @@ int main()
     struct tm *sp;
 
     if (time(&now) == -1){
-	    perror("Time error");
+	    perror("Error");
+	    exit(1);
     }
     
-    //putenv("TZ=PST8PDT");
+    /*if (putenv("TZ=PST8PDT") == -1){
+	    perror("Error");
+	    exit(1);
+    }*/
+    if (setenv("TZ", "PST8PDT", 1) != 0){
+	    perror("Error");
+	    exit(1);
+    }
+
     printf("%s", ctime( &now ) );
     sp = localtime(&now);
     printf("%d/%d/%02d %d:%02d %s\n",
             sp->tm_mon + 1, sp->tm_mday,
-            sp->tm_year, sp->tm_hour,
+            sp->tm_year+1900, sp->tm_hour,
             sp->tm_min, tzname[sp->tm_isdst]);
     return(0);
 }
