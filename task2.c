7eb5deb4a4d70491ae8a5eb3a94ab36cbe961be5
Lekvf <92939783+Lekvf@users.noreply.github.com>
2022-02-14 14:39:37 +0700

    Update task2.c

--- task2.c
+++ task2.c
@@ -22,7 +22,7 @@ int main()
 
     printf("%s", ctime( &now ) );
     sp = localtime(&now);
-    printf("%d/%d/%02d %d:%02d %s\n",
+    printf("%02d/%d/%02d %d:%02d %s\n",
             sp->tm_mon + 1, sp->tm_mday,
             sp->tm_year+1900, sp->tm_hour,
             sp->tm_min, tzname[sp->tm_isdst]);
