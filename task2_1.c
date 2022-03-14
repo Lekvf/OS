32c4c2ee38283da2b60ff13b486c22c6a06e594a
Lekvf <92939783+Lekvf@users.noreply.github.com>
2022-03-14 18:23:31 +0700

    Update task2_1.c

--- task2_1.c
+++ task2_1.c
@@ -3,11 +3,11 @@
 #include <time.h>
 #include <stdlib.h>
 
-#define ERROR -1
-#define TIME_ERROR 1
-#define SETENV_MEMORY_ALLOCATE_ERROR 2
-#define CTIME_ERROR 3
-#define LOCALTIME_ERROR 4
+enum Errors{
+        ERROR = -1,
+        TIME_ERROR = 1,
+        SETENV_ERROR
+};
 
 extern char *tzname[];
 
@@ -17,9 +17,7 @@ int main()
     struct tm *sp;
     char *Ctime;
 
-    int err;
-
-    err = time(&now);
+    int err = time(&now);
     if (err == ERROR){
             perror("Error in function time");
             exit(TIME_ERROR);
@@ -28,20 +26,20 @@ int main()
     err = setenv("TZ", "PST8PDT", 1);
     if (err == ERROR){
             perror("Error in function setenv");
-            exit(SETENV_MEMORY_ALLOCATE_ERROR);
+            exit(SETENV_ERROR);
     }
 
     Ctime = ctime(&now);
     if (!Ctime){
             perror("Error in function ctime");
-            exit(CTIME_ERROR);
+            exit(TIME_ERROR);
     }
     printf("%s", Ctime);
 
     sp = localtime(&now);
     if (!sp){
             perror("Error in function localtime");
-            exit(LOCALTIME_ERROR);
+            exit(TIME_ERROR);
     }
 
     printf("%02d/%d/%02d %d:%02d:%02d %s\n",
@@ -51,4 +49,4 @@ int main()
 
     return(0);
 }
-~           
+       
