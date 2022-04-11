da0f15b05966435ec57d57e3043c80d30a91ff76
Lekvf <92939783+Lekvf@users.noreply.github.com>
2022-04-11 15:38:47 +0700

    Update lab10.c

--- lab10/lab10.c
+++ lab10/lab10.c
@@ -3,7 +3,9 @@
 #include <stdio.h>
 
 #define ERROR -1
-#define FORK_ERROR -2
+#define FORK_ERROR 2
+#define EXEC_ERROR 3
+#define WAIT_ERROR 4
 #define SUCCESSFUL_COMPLETION 0
 #define NO_ARGUMENTS 1
 #define I_AM_CHILD_PROCCESS 0
@@ -23,24 +25,24 @@ int main(int argc, char *argv[]) {
 	}
 
 	if (pid == I_AM_CHILD_PROCCESS){
-		int err = execvp(argv[1], argv + 1);
-		
+		int err = execvp(argv[1], &argv[1]);
 		if (err == ERROR){
 			perror("error in function exec");
-			return ERROR;
+			return EXEC_ERROR;
 		}
 	}
 	
 	int statloc;
 	pid = wait(&statloc);
-	
+
 	if (pid == ERROR){
 		perror("Error in function wait");
-		return ERROR;
+		return WAIT_ERROR;
 	}
 	
 	if (WIFEXITED(statloc)) {
-		printf("proccess with pid = %d\n finished with status %d\n", pid, WEXITSTATUS(statloc));
+		printf("\nPID of child proccess is %d\n", pid);
+		printf("Finished with %d status\n", WEXITSTATUS(statloc) );
 	}
 
 	return 0;
