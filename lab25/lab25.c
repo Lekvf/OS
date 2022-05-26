44acfbc38401c912c92f13a9271169ecf77a3027
Lekvf <92939783+Lekvf@users.noreply.github.com>
2022-05-26 15:49:46 +0700

    Update lab25.c

--- lab25/lab25.c
+++ lab25/lab25.c
@@ -10,100 +10,12 @@
 #define FOR_READ 0
 #define FOR_WRITE 1
 
-int pipeReader(int *fd){
-	char c;
-	int err, length;
-	do {
-		length = read(fd[FOR_READ], &c, 1);
-		if (length == ERROR){
-			perror("read error");
-			return ERROR;
-		}
-		c = toupper(c);
-		
-		err = write(STDOUT_FILENO, &c, 1);
-		if (err == ERROR){
-			perror("write error");
-			return ERROR;
-		}
-	} while (length > 0);
-	
-	err = close(fd[FOR_READ]);
-	if (err == ERROR){
-		perror("close error");
-		return ERROR;
-	}
-	
-	return SUCCESS;
-}
-
-int pipeWriter(int *fd){
-	char c;
-	int err, length;	
-	do{
-		length = read(STDIN_FILENO, &c, 1);
-		if (length == ERROR){
-			perror("read error");
-			return ERROR;
-		}
-		
-		err = write(fd[FOR_WRITE], &c, 1);
-		if (err == ERROR){
-			perror("write error");
-			return ERROR;
-		}
-	} while(length > 0);
-	
-	err = close(fd[FOR_WRITE]);
-	if (err == ERROR){
-		perror("close error");
-		return ERROR;
-	}
-	
-	return SUCCESS;
-}
-
-int readerChild(int *fd){
-	int err;
-	
-	err = close(fd[FOR_WRITE]);
-	if (err == ERROR){
-		perror("close error");
-		return ERROR;
-	}	
-	err = pipeReader(fd);
-	if (err == ERROR){
-		return ERROR;
-	}
-	
-	return SUCCESS;
-}
-
-int writerChild(int *fd){
-	int err;
-	
-	err = close(fd[FOR_READ]);
-	if (err == ERROR){
-		perror("close error");
-		return ERROR;
-	}
-	err = pipeWriter(fd);
-	if (err == ERROR){
-		return ERROR;
-	}
-	return SUCCESS;
-}
-
-void closeBothfd(int *fd){
-	int err = close(fd[FOR_READ]);
-	if (err == ERROR){
-		perror("close error");
-	}
-	err = close(fd[FOR_WRITE]);
-	if (err == ERROR){
-		perror("close error");
-	}
-}
+int closeFD(int fd);
+int closeBothFD(int *fd);
+int readFromPipe(int *fd);
+int writeInPipe(int *fd);
+int reader(int *fd);
+int writer(int *fd);
 
 int main(){
 	int fd[2];
@@ -113,15 +25,14 @@ int main(){
 		perror("pipe error");
 		return ERROR;
 	}
-
+	
 	pid_t pid = fork();
 	if (pid == ERROR){
 		perror("fork error");
-		closeBothfd(fd);
 		return ERROR;
 	}
 	if (pid == CHILD_PROCCESS){
-		err = readerChild(fd);
+		err = writer(fd);
 		if (err == ERROR)
 			return ERROR;
 		return SUCCESS;
@@ -130,21 +41,130 @@ int main(){
 	pid = fork();
 	if (pid == ERROR){
 		perror("fork error");
-		closeBothfd(fd);
 		return ERROR;
 	}
-	
 	if (pid == CHILD_PROCCESS){
-		err = writerChild(fd);
+		err = reader(fd);
 		if (err == ERROR)
 			return ERROR;
 		return SUCCESS;
 	}
-
-	pid = wait(NULL);
-	if (pid == ERROR){
-		perror("Error in function wait");
+	
+	err = closeBothFD(fd);
+	if (err == ERROR) return ERROR;
+	int stat;
+	int childNum = 0;
+	while ((pid = wait(&stat)) > 0) {
+		childNum++;
+	}
+	if (childNum != 2){
+		perror("wait error");
 		return ERROR;
 	}
+
 	return 0;
+}
+
+int closeFD(int fd){
+	int err = close(fd);
+	if (err == ERROR){
+		perror("close error");
+		return ERROR;
+	}
+	return SUCCESS;
+}
+
+int closeBothFD(int *fd){
+	int err = close (fd[FOR_READ]);
+	if (err == ERROR){
+		perror("close error");
+		return ERROR;
+	}
+	err = close(fd[FOR_WRITE]);
+	if (err == ERROR){
+		perror("close error");
+		return ERROR;
+	}
+	
+	return SUCCESS;
+}
+
+int readFromPipe(int *fd){
+	char c;
+	int err, length;
+	do {
+		length = read(fd[FOR_READ], &c, 1);
+		if (length == ERROR){
+			perror("read error");
+			return ERROR;
+		}
+		if (length == 0) break;
+		c = toupper(c);
+
+		err = write(STDOUT_FILENO, &c, 1);
+		if (err == ERROR){
+			perror("write error");
+			return ERROR;
+		}
+	} while (length > 0);
+	return SUCCESS;
+}
+
+int writeInPipe(int *fd){
+	char c;
+	int err, length;	
+	do{
+		length = read(STDIN_FILENO, &c, 1);
+		if (length == ERROR){
+			perror("read error");
+			return ERROR;
+		}
+		if (length == 0) break;
+		err = write(fd[FOR_WRITE], &c, 1);
+		if (err == ERROR){
+			perror("write error");
+			return ERROR;
+		}
+	} while (length > 0);
+	
+	return SUCCESS;
+}
+
+int reader(int *fd){
+	int err;
+	
+	err = closeFD(fd[FOR_WRITE]);
+	if (err == ERROR){
+		return ERROR;
+	}
+	err = readFromPipe(fd);
+	if (err == ERROR){
+		return ERROR;
+	}
+	
+	err = closeFD(fd[FOR_READ]);
+	if (err == ERROR){
+		return ERROR;
+	}
+	return SUCCESS;
+}
+
+int writer(int *fd){
+	int err;
+	
+	err = closeFD(fd[FOR_READ]);
+	if (err == ERROR){
+		return ERROR;
+	}
+	
+	err = writeInPipe(fd);
+	if (err == ERROR){
+		return ERROR;
+	}
+	
+	err = closeFD(fd[FOR_WRITE]);
+	if (err == ERROR){
+		return ERROR;
+	}
+	return SUCCESS;
 }
